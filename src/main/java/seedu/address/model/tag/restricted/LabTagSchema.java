99ca77cb9255640bd9d44d37930c73269d9547a3
lolepop <8401103+lolepop@users.noreply.github.com>
2026-04-13 01:31:46 +0800

    Improve message format style

--- src/main/java/seedu/address/model/tag/restricted/LabTagSchema.java
+++ src/main/java/seedu/address/model/tag/restricted/LabTagSchema.java
@@ -14,7 +14,8 @@ public class LabTagSchema extends RegexTagSchema {
     public static final String TAG_PATTERN = "[A-Z]?\\d{1,2}(-" + CourseTagSchema.TAG_PATTERN + ")?";
     public static final String MESSAGE_CONSTRAINTS = "Lab tag expects format of an optional uppercase letter "
             + "followed by maximum 2 numbers.\nOPTIONAL: an associated course is attachable at the end "
-            + "with a dash (-). " + CourseTagSchema.MESSAGE_CONSTRAINTS + "\nValid lab: 'D24-CS2103T' and '8'";
+            + "with a dash (-). " + CourseTagSchema.MESSAGE_CONSTRAINTS + "\n"
+            + "Valid lab: 'D24-CS2103T' and '8'";
 
     /**
      * Constructs a {@code LabTagSchema}.
